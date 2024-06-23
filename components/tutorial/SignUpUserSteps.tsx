fa4bc20a502be5a98bab3ad6d40656eb64664b7d
xynrgy <70825837+xynrgys@users.noreply.github.com>
2024-06-23 14:27:49 -0700

    adding fastapi

--- components/tutorial/SignUpUserSteps.tsx
+++ components/tutorial/SignUpUserSteps.tsx
@@ -16,6 +16,11 @@ export default function SignUpUserSteps() {
           page and sign up your first user. It's okay if this is just you for
           now. Your awesome idea will have plenty of users later!
         </p>
+        <div className="mb-6">
+            <Link href="/api/python">
+              <code className="font-mono text-white font-bold">api/index.py</code>
+            </Link>
+          </div>
       </Step>
     </ol>
   );
