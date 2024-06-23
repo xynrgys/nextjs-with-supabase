051401e0f918d033c2fcd09402596d4d75b966cb
xynrgy <70825837+xynrgys@users.noreply.github.com>
2024-06-23 16:49:01 -0700

    package and config fixes

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    experimental: {
+      serverActions: true, // Add this line
+    },
     rewrites: async () => {
       return [
         {
