acd4935f959851df56e1bcc1a16ba81edb758680
xynrgy <70825837+xynrgys@users.noreply.github.com>
2024-06-23 21:13:38 -0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -38,15 +38,7 @@ export default async function Index() {
 
       <footer className="w-full border-t border-t-foreground/10 p-8 flex justify-center text-center text-xs">
         <p>
-          Powered by{" "}
-          <a
-            href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
-            target="_blank"
-            className="font-bold hover:underline"
-            rel="noreferrer"
-          >
-            Supabase
-          </a>
+          Made with love by Young
         </p>
       </footer>
     </div>
