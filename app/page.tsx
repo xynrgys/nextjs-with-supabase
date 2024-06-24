295b6957e85c2149404226333a6d798759cce812
xynrgy <70825837+xynrgys@users.noreply.github.com>
2024-06-23 21:11:39 -0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -23,7 +23,7 @@ export default async function Index() {
     <div className="flex-1 w-full flex flex-col gap-20 items-center">
       <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
         <div className="w-full max-w-4xl flex justify-between items-center p-3 text-sm">
-          <DeployButton />
+          <h3 className="font-bold text-white text-xl">Serpent</h3>
           {isSupabaseConnected && <AuthButton />}
         </div>
       </nav>
