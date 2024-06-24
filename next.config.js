ece0880cbd91c5f155160c4a9be803538c69e91d
xynrgy <70825837+xynrgys@users.noreply.github.com>
2024-06-23 17:35:14 -0700

    Revert "package and config fixes"
    This reverts commit 051401e0f918d033c2fcd09402596d4d75b966cb.

--- next.config.js
+++ next.config.js
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-      serverActions: true, // Add this line
-    },
     rewrites: async () => {
       return [
         {
